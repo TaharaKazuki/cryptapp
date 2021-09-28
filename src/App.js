f4179aaf4f774c7fcd0058c985ecc8c0149ccdb8
田原 一樹 <kazuki.tahara@rarejob.co.jp>
2021-09-28 13:19:16 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react'
-// import { Switch, Route, Link } from 'react-router-dom'
-// import { Layout, Typography, Space } from 'antd'
+import { Switch, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
+
+// components 
+import { Homepage } from './components'
 
 
 const App = () => {
   return (
-    <div>
-      <h1>Cryptoverse</h1>
+    <div className="app">
+      <div className="navbar">
+
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+
+              </Route>
+
+            </Switch>
+          </div>
+        </Layout>
+      </div>
+      <div className="footer">
+
+      </div>
     </div>
   )
 }

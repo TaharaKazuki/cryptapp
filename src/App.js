ff1d27c7fbe20730f68fdf546da64587ddf13671
田原 一樹 <kazuki.tahara@rarejob.co.jp>
2021-09-28 14:54:15 +0900

    update

--- src/App.js
+++ src/App.js
@@ -3,29 +3,50 @@ import { Switch, Route, Link } from 'react-router-dom'
 import { Layout, Typography, Space } from 'antd'
 
 // components 
-import { Homepage } from './components'
+import { Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails, Navbar } from './components'
 
 
 const App = () => {
   return (
     <div className="app">
       <div className="navbar">
-
+        <Navbar />
       </div>
       <div className="main">
         <Layout>
           <div className="routes">
             <Switch>
               <Route exact path="/">
-
+                <Homepage />
               </Route>
-
+              <Route exact path="/exchanges">
+                <Exchanges />
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path="/cryptp/:coinId">
+                <CryptoDetails />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
             </Switch>
           </div>
         </Layout>
       </div>
       <div className="footer">
-
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+          <Link to="/">
+            Cryptoverse
+          </Link><br />
+          All Rights Reserved.
+        </Typography.Title>
+        <Space>
+          <Link to="/">Home</Link>
+          <Link to="/exchanges">Exchanges</Link>
+          <Link to="/news">News</Link>
+        </Space>
       </div>
     </div>
   )

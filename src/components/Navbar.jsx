d340092b7870dce4528df0251d8fbb56596b0771
田原 一樹 <kazuki.tahara@rarejob.co.jp>
2021-09-29 16:21:03 +0900

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd'
 import { Link } from 'react-router-dom'
 import {
@@ -12,6 +12,20 @@ import {
 import icon from '../images/cryptocurrency.png'
 
 const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState(true)
+  const [screenSize, setScreenSize] = useState(undefined)
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth)
+    window.addEventListener('resize', handleResize)
+    handleResize()
+    return () => window.removeEventListener('resize', handleResize)
+  },[])
+
+  useEffect(() => {
+    screenSize <= 800 ? setActiveMenu(false) : setActiveMenu(true)
+  }, [screenSize])
+
   return (
     <div className="nav-container">
       <div className="logo-container">
@@ -19,7 +33,7 @@ const Navbar = () => {
         <Typography.Title level={2} className="logo">
           <Link to="/">Cryptoverse</Link>
         </Typography.Title>
-        <Button className="menu-control-container">
+        <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}>
           <MenuOutlined />
         </Button>
       </div>
